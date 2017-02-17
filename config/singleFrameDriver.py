07a405dd2a197fa05ee1f1afc8ce9d8fc9104add
Robert <rbliu@users.noreply.github.com>
2017-02-17 11:34:42 -0800

    Update singleFrameDriver.py

--- config/singleFrameDriver.py
+++ config/singleFrameDriver.py
@@ -2,5 +2,5 @@ import os.path
 
 from lsst.utils import getPackageDir
 
-config.processCcd.load(os.path.join(getPackageDir("obs_cfht"), "config", "processCcd.py"))
+config.processCcd.load(os.path.join(getPackageDir("obs_generic"), "config", "processCcd.py"))
 config.ccdKey = 'ccd'
